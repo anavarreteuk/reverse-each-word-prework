f02a95ac54b18f58002b9efad65e921a27744b26
anavarreteuk <a.navarrete.uk@gmail.com>
2018-09-03 11:41:50 +0100

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,21 @@
-def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+#def reverse_each_word(sentence)
+  #sentence.split.collect {|word| word.reverse}.join(" ")
+#end
+
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  reversed_array = []
+  array.collect do |item|
+    reversed_array.push(item.reverse)
+  end
+  return reversed_array.join(" ")
 end
